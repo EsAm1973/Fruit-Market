4848f38ad100754ed78080da7d90a6c4c6e88b7b
EsAm1973 <me354172@gmail.com>
2025-07-06 21:11:28 +0300

    Add Cairo font assets and define text styles for consistent typography

---
+++ lib/Core/utils/app_text_styles.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  static const TextStyle regular11 = TextStyle(
+    fontSize: 11,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semibold11 = TextStyle(
+    fontSize: 11,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bold11 = TextStyle(
+    fontSize: 11,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle regular13 = TextStyle(
+    fontSize: 13,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semibold13 = TextStyle(
+    fontSize: 13,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bold13 = TextStyle(
+    fontSize: 13,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle regular16 = TextStyle(
+    fontSize: 16,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semibold16 = TextStyle(
+    fontSize: 16,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bold16 = TextStyle(
+    fontSize: 16,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle regular19 = TextStyle(
+    fontSize: 19,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semibold19 = TextStyle(
+    fontSize: 19,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bold19 = TextStyle(
+    fontSize: 19,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle regular23 = TextStyle(
+    fontSize: 23,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semibold23 = TextStyle(
+    fontSize: 23,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bold23 = TextStyle(
+    fontSize: 23,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle regular28 = TextStyle(
+    fontSize: 28,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semibold28 = TextStyle(
+    fontSize: 28,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bold28 = TextStyle(
+    fontSize: 28,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle regular33 = TextStyle(
+    fontSize: 33,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semibold33 = TextStyle(
+    fontSize: 33,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bold33 = TextStyle(
+    fontSize: 33,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle regular40 = TextStyle(
+    fontSize: 40,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semibold40 = TextStyle(
+    fontSize: 40,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bold40 = TextStyle(
+    fontSize: 40,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle regular48 = TextStyle(
+    fontSize: 48,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semibold48 = TextStyle(
+    fontSize: 48,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bold48 = TextStyle(
+    fontSize: 48,
+    decoration: TextDecoration.none,
+    fontFamily: 'Cairo',
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+  );
+}
