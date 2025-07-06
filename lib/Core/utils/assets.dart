7c45fd278c9716e73eef15a49a119f4a09adc61c
EsAm1973 <me354172@gmail.com>
2025-07-06 17:52:15 +0300

    Add new image assets for pageview backgrounds and images

--- lib/Core/utils/assets.dart
+++ lib/Core/utils/assets.dart
@@ -14,6 +14,22 @@ class Assets {
   /// assets/images/logo_splash.svg
   static const String imagesLogoSplash = "assets/images/logo_splash.svg";
 
+  /// Assets for imagesPageviewBackground1
+  /// assets/images/pageview_background1.svg
+  static const String imagesPageviewBackground1 = "assets/images/pageview_background1.svg";
+
+  /// Assets for imagesPageviewBackground2
+  /// assets/images/pageview_background2.svg
+  static const String imagesPageviewBackground2 = "assets/images/pageview_background2.svg";
+
+  /// Assets for imagesPageviewImage1
+  /// assets/images/pageview_image1.svg
+  static const String imagesPageviewImage1 = "assets/images/pageview_image1.svg";
+
+  /// Assets for imagesPageviewImage2
+  /// assets/images/pageview_image2.svg
+  static const String imagesPageviewImage2 = "assets/images/pageview_image2.svg";
+
   /// Assets for imagesPlantSplash
   /// assets/images/plant_splash.svg
   static const String imagesPlantSplash = "assets/images/plant_splash.svg";
