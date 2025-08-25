ff36fbe1650c401b253f962e919f94e919945b04
EsAm1973 <me354172@gmail.com>
2025-08-25 19:06:24 +0300

    Add OrderEntity and ShippimgAddressEntity classes for checkout process

---
+++ lib/Features/checkout/domain/entities/shippimg_address_entity.dart
@@ -0,0 +1,17 @@
+class ShippimgAddressEntity {
+  final String name;
+  final String address;
+  final String city;
+  final String email;
+  final String phone;
+  final String addressDescription;
+
+  ShippimgAddressEntity({
+    required this.name,
+    required this.address,
+    required this.city,
+    required this.email,
+    required this.phone,
+    required this.addressDescription,
+  });
+}
